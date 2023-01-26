64d3487c954119db71b70d91bbafac6cc0ff3889
Lucas Rodrigues <lucasro229@gmail.com>
2023-01-26 20:35:11 -0300

    fix: update PageHeader test

--- src/components/page-header/page-header.spec.tsx
+++ src/components/page-header/page-header.spec.tsx
@@ -1,14 +1,14 @@
 import { screen, render } from '@testing-library/react';
 import { PageHeader } from '.';
 
-describe('CategoriaForm', () => {
+describe('PageHeader', () => {
   it('has the correct data', () => {
     render(
-      <PageHeader title="Título" subtitle="Subtítulo" children="Children" />
+      <PageHeader title="Titulo" subtitle="Subtitulo"><p>Children</p></PageHeader>
     );
 
-    expect(screen.getByRole('heading')).toHaveTextContent('Título');
-    expect(screen.getByText('Subtítulo')).toBeInTheDocument();
+    expect(screen.getByRole('heading')).toHaveTextContent('Titulo');
+    expect(screen.getByText('Subtitulo')).toBeInTheDocument();
     expect(screen.getByText('Children')).toBeInTheDocument();
   });
 });
