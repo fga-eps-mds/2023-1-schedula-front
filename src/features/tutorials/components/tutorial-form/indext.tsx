c5ea4460181f41252e782252a5ab04687aa3d131
Júlia <contato.juliasm@gmail.com>
2023-05-14 21:50:24 -0400

    feat: #28 fazendo alterações no formulários de criar tutoriais
    Co-authored-by: cwtshh <gucosta1719@gmail.com>

--- src/features/tutorials/components/tutorial-form/indext.tsx
+++ src/features/tutorials/components/tutorial-form/indext.tsx
@@ -1,19 +1,39 @@
-import { Button, GridItem } from '@chakra-ui/react';
+import { Button, Select, Grid } from '@chakra-ui/react';
 import { ControlledSelect, Input } from '@/components/form-fields';
 
 export function TutorialForm() {
   return (
     <form>
       <Input label="Nome do tutorial" errors={undefined} />
+      <div>
+        <span>Categoria</span>
+        <Select
+          className="select"
+          placeholder="Selecione a Categoria"
+          style={{ marginTop: 8 }}
+        >
+          <option value="Criação de ponto de rede">
+            Criação de ponto de rede
+          </option>
+          <option value="Redefinição de senha">Redefinição de senha</option>
+        </Select>
+      </div>
 
-      <Input label="Categoria" errors={undefined} />
+      <Input
+        label="Adicione o arquivo"
+        type="file"
+        errors={undefined}
+        style={{ marginTop: 3 }}
+      />
 
-      <GridItem colSpan={2}>
-        <Button type="submit" size="lg" width="100%">
+      <Grid templateColumns="1fr 0fr" style={{ marginTop: 12 }}>
+        <Button type="submit" size="lg" width="45%">
           Criar Tutorial
         </Button>
-        <Button type="submit">Cancelar</Button>
-      </GridItem>
+        <Button type="submit" size="lg" width="45%">
+          Cancelar
+        </Button>
+      </Grid>
     </form>
   );
 }
