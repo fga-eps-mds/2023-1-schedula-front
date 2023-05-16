3cdc457254900c199a91cc87e985a821bbe5cc42
Amanda Abreu <103958998+Amandaaaaabreu@users.noreply.github.com>
2023-05-15 23:05:20 -0300

    feat: add search filters
    Co-authored-by: guilhermemoraisr <guilhermemoraisr@gmail.com>
    Co-authored-by: arthurrsousa <992arthur@gmail.com>

--- src/pages/tutoriais/index.tsx
+++ src/pages/tutoriais/index.tsx
@@ -1,14 +1,30 @@
-import { useCallback, useState } from 'react';
-import { Button, HStack, useDisclosure } from '@chakra-ui/react';
+import { useCallback, useState, useEffect } from 'react';
+import {
+  Button,
+  HStack,
+  useDisclosure,
+  Input,
+  Select,
+  Grid,
+  GridItem,
+} from '@chakra-ui/react';
 import { PageHeader } from '@/components/page-header';
 import { useGetAllTutorials } from '@/features/tutorials/api/get-all-tutorials';
 import { ListView } from '@/components/list';
 import { TutorialItem } from '@/features/tutorials/components/tutorial-item';
 import { useDeleteTutorial } from '@/features/tutorials/api/delete-tutorial';
 import { Tutorial } from '@/features/tutorials/api/types';
+import {
+  chakraStyles,
+  customComponents,
+} from '@/components/form-fields/controlled-select/styles';
 
 export function Tutoriais() {
   const { data: tutorials, isLoading, refetch } = useGetAllTutorials();
+  const [filteredTutorials, setFilteredTutorials] = useState<Tutorial[]>(
+    tutorials || []
+  );
+  const [selectedState, setSelectedState] = useState<string>('');
 
   const { mutate: deleteTutorial, isLoading: isRemovingTutorial } =
     useDeleteTutorial();
@@ -44,12 +60,69 @@ export function Tutoriais() {
     [onDelete, onEdit, isRemovingTutorial]
   );
 
+  const handleSearch = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const searchText = event.target.value.toLowerCase();
+      const filteredTutorials = tutorials?.filter((tutorial) =>
+        tutorial.name.toLowerCase().includes(searchText)
+      );
+      setFilteredTutorials(filteredTutorials || []);
+    },
+    [tutorials]
+  );
+
+  const handleStateChange = useCallback(
+    (event: React.ChangeEvent<HTMLSelectElement>) => {
+      const selectedState = event.target.value;
+      setSelectedState(selectedState);
+    },
+    []
+  );
+
+  const uniqueCategories = new Set(
+    tutorials?.map((tutorial) => tutorial.state)
+  );
+
+  console.log(uniqueCategories);
+
+  useEffect(() => {
+    let updatedTutorials = tutorials || [];
+    if (selectedState) {
+      updatedTutorials = updatedTutorials.filter(
+        (tutorial) => tutorial.state === selectedState
+      );
+    }
+    setFilteredTutorials(updatedTutorials);
+  }, [tutorials, selectedState]);
+
   return (
     <>
       <PageHeader title="Tutoriais" />
 
+      <Grid templateColumns="repeat(2, 1fr)" gap={8}>
+        <Input
+          placeholder="Pesquisar tutoriais"
+          onChange={handleSearch}
+          marginBottom="4"
+        />
+
+        <Select
+          placeholder="Filtrar por estado"
+          onChange={handleStateChange}
+          value={selectedState}
+          chakraStyles={chakraStyles}
+          marginBottom="4"
+        >
+          {[...uniqueCategories].map((state) => (
+            <option key={state} value={state}>
+              {state}
+            </option>
+          ))}
+        </Select>
+      </Grid>
+
       <ListView<Tutorial>
-        items={tutorials}
+        items={filteredTutorials}
         render={renderTutorialItem}
         isLoading={isLoading}
       />
