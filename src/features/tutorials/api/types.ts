754cd98597432e70db86a914972c8e4636c951df
FauseSkyWalker <Fausejr@gmail.com>
2023-05-17 21:53:57 -0300

    feat: Organization and exclusion of some code elements (States:Strings)
    Co-authored-by: MatsFidelis <200073184@aluno.unb.br>
    Co-authored-by: gabrielbpn <gabrielbpn0802@gmail.com>

--- src/features/tutorials/api/types.ts
+++ src/features/tutorials/api/types.ts
@@ -1,32 +1,27 @@
 export interface Tutorial {
   id: string;
   name: string;
-  state: string;
 }
 
 export interface PostCreateTutorialParams {
   name: string;
-  state: string;
 }
 
 export interface PostCreateTutorialResponse {
   id: string;
   name: string;
-  state: string;
 }
 
 export interface PutUpdateTutorialParams {
   tutorialId: string;
   data: {
     name: string;
-    state: string;
   };
 }
 
 export interface PutUpdateTutorialResponse {
   id: string;
   name: string;
-  state: string;
 }
 
 export interface DeleteTutorialParams {
