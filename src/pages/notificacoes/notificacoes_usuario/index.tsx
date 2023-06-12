9c54e6dc90e0483f80ce43983f1f078323f3ed24
cwtshh <gucosta1719@gmail.com>
2023-06-12 15:07:41 -0300

    #41 - Adding search bar to notification page
    Co-authored-by: MatsFidelis <200073184@aluno.unb.br>
    Co-authored-by: IanFPFerreira <ianfillipe@gmail.com>

--- src/pages/notificacoes/notificacoes_usuario/index.tsx
+++ src/pages/notificacoes/notificacoes_usuario/index.tsx
@@ -1,5 +1,31 @@
+import { FaSearch } from 'react-icons/fa';
+import {
+  Button,
+  Grid,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Icon,
+  Flex,
+} from '@chakra-ui/react';
 import { PageHeader } from '@/components/page-header';
 
 export function NotificacaoUsuario() {
-  return <PageHeader title="Notificações" />;
+  return (
+    <>
+      <PageHeader title="Notificações" />
+
+      <Grid>
+        <InputGroup>
+          <InputLeftElement pointerEvents="none">
+            <Icon as={FaSearch} boxSize={4} color="gray.400" />
+          </InputLeftElement>
+          <Input
+            placeholder="Pesquisar notificações"
+            _placeholder={{ color: 'gray.400' }}
+          />
+        </InputGroup>
+      </Grid>
+    </>
+  );
 }
