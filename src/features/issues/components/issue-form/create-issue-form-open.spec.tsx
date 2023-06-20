594fcdfb5a19e15f676aaf503e55b9ad4ee8a606
FauseSkyWalker <Fausejr@gmail.com>
2023-06-20 14:55:48 -0300

    increasing tests
    Co-authored-by: italofernandes13 <italofernandes13@users.noreply.github.com>
    Co-authored-by: Amanda Abreu <Amandaaaaabreu@users.noreply.github.com>

--- src/features/issues/components/issue-form/create-issue-form-open.spec.tsx
+++ src/features/issues/components/issue-form/create-issue-form-open.spec.tsx
@@ -6,7 +6,6 @@ import { theme } from '@/styles/theme';
 import { CreateIssueForm } from './create-issue-form-open';
 import { AuthProvider } from '@/contexts/AuthContext';
 
-// eslint-disable-next-line import/no-extraneous-dependencies
 import 'intersection-observer';
 
 describe('renders form fields correctly', () => {
@@ -97,8 +96,50 @@ describe('renders form fields correctly', () => {
     });
 
     fireEvent.click(screen.getByText('Registrar Agendamento'));
-    // expect(
-    //   screen.getByLabelText('Formato de e-mail inválido')
-    // ).toBeInTheDocument();
+    await expect(screen.findByText('Formato de e-mail inválido'));
+  });
+
+  it('displays a success message after submitting the form', async () => {
+    render(
+      <BrowserRouter>
+        <AuthProvider>
+          <ChakraProvider resetCSS theme={theme}>
+            <QueryClientProvider client={queryClient}>
+              <CreateIssueForm />
+            </QueryClientProvider>
+          </ChakraProvider>
+        </AuthProvider>
+      </BrowserRouter>
+    );
+
+    fireEvent.change(screen.getByLabelText('Solicitante'), {
+      target: { value: 'Mocked Requester' },
+    });
+    fireEvent.change(screen.getByLabelText('Celular'), {
+      target: { value: '61123456789' },
+    });
+    fireEvent.change(screen.getByLabelText('Email'), {
+      target: { value: 'mocked@policiacivil.go.gov.br' },
+    });
+    fireEvent.change(screen.getByLabelText('Cidade'), {
+      target: { value: 'Mocked City' },
+    });
+    fireEvent.change(screen.getByLabelText('Posto de Trabalho'), {
+      target: { value: 'Mocked Workstation' },
+    });
+    fireEvent.change(screen.getByLabelText('Telefone'), {
+      target: { value: '12345678912' },
+    });
+    fireEvent.change(screen.getByLabelText('Categoria do Problema'), {
+      target: { value: 'Mocked Problem Categorie' },
+    });
+    fireEvent.change(screen.getByLabelText('Tipos de Problema'), {
+      target: { value: 'Mocked Problem Types' },
+    });
+    fireEvent.change(screen.getByLabelText('Descrição do Problema'), {
+      target: { value: 'Mocked Problem Description' },
+    });
+    fireEvent.click(screen.getByText('Registrar Agendamento'));
+    await expect(screen.findByText('Ir para os agendamentos'));
   });
 });
