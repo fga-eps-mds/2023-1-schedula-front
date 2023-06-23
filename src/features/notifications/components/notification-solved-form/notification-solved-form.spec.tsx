4e51a0355d1f24266ae018a36136f3688f89bb97
MatsFidelis <200073184@aluno.unb.br>
2023-06-23 18:00:13 -0300

    [US10] - Adding tests - Forms notification
    Co-authored-by: cwtshh <gucosta1719@gmail.com>
    Co-authored-by: IanFPFerreira <ianfillipe@gmail.com>

---
+++ src/features/notifications/components/notification-solved-form/notification-solved-form.spec.tsx
@@ -0,0 +1,46 @@
+import {
+  screen,
+  render,
+  waitFor,
+  act,
+  fireEvent,
+} from '@testing-library/react';
+import { vi } from 'vitest';
+import { NotificationSolvedForm } from '.';
+import { Notification } from '../../types';
+
+const mockedNotification: Notification = {
+  id: '1',
+  sourceEmail: 'email@gmail.com',
+  sourceName: 'name',
+  targetName: 'targetName',
+  targetEmail: 'email1@gmail.com',
+  message: 'message',
+  status: 'pending',
+  pendency: 'pending',
+  read: false,
+  createdAt: new Date(),
+};
+
+const mockedOnSubmit = vi.fn(() => {});
+
+describe('NotificationSolvedForm', () => {
+  it('should be able to updtate notification to Solved', async () => {
+    render(
+      <NotificationSolvedForm
+        defaultValues={mockedNotification}
+        onSubmit={mockedOnSubmit}
+        isSubmitting={false}
+        onClose={() => {}}
+      />
+    );
+
+    await act(async () => {
+      screen.getByText('Adicionar').click();
+    });
+
+    await waitFor(() => {
+      expect(mockedOnSubmit).toHaveBeenCalled();
+    });
+  });
+});
