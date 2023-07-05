9588e4714e57d2059f3300b205557feff6def1f2
Guilherme de Morais Richter <guilhermemoraisr@gmail.com>
2023-07-05 17:38:53 -0300

    fix: make changes to the typecheck
    Co-authored-by: arthurrsousa <992arthur@gmail.com>
    Co-authored-by: Amandaaaaabreu <amandaabreu045@gmail.com>

--- src/pages/chamados/chamados.spec.tsx
+++ src/pages/chamados/chamados.spec.tsx
@@ -100,25 +100,160 @@ describe('sortIssues', () => {
 
   it('should sort the issues in descending order based on the date', () => {
     const issues: Issue[] = [
-      { id: '1', date: '2021-09-01' },
-      { id: '2', date: '2021-08-15' },
-      { id: '3', date: '2021-08-30' },
+      {
+        id: '1',
+        date: new Date('2021-09-01'),
+        requester: '',
+        phone: '',
+        city_id: '',
+        workstation_id: '',
+        email: '',
+        problem_category: {
+          id: '',
+          name: '',
+          description: '',
+          problem_types: [],
+        },
+        problem_types: [],
+      },
+      {
+        id: '2',
+        date: new Date('2021-08-15'),
+        requester: '',
+        phone: '',
+        city_id: '',
+        workstation_id: '',
+        email: '',
+        problem_category: {
+          id: '',
+          name: '',
+          description: '',
+          problem_types: [],
+        },
+        problem_types: [],
+      },
+      {
+        id: '3',
+        date: new Date('2021-08-30'),
+        requester: '',
+        phone: '',
+        city_id: '',
+        workstation_id: '',
+        email: '',
+        problem_category: {
+          id: '',
+          name: '',
+          description: '',
+          problem_types: [],
+        },
+        problem_types: [],
+      },
     ];
 
     const sortedIssues = sortIssues(issues);
 
     expect(sortedIssues).toEqual([
-      { id: '1', date: '2021-09-01' },
-      { id: '3', date: '2021-08-30' },
-      { id: '2', date: '2021-08-15' },
+      {
+        id: '1',
+        date: new Date('2021-09-01'),
+        requester: '',
+        phone: '',
+        city_id: '',
+        workstation_id: '',
+        email: '',
+        problem_category: {
+          id: '',
+          name: '',
+          description: '',
+          problem_types: [],
+        },
+        problem_types: [],
+      },
+      {
+        id: '3',
+        date: new Date('2021-08-30'),
+        requester: '',
+        phone: '',
+        city_id: '',
+        workstation_id: '',
+        email: '',
+        problem_category: {
+          id: '',
+          name: '',
+          description: '',
+          problem_types: [],
+        },
+        problem_types: [],
+      },
+      {
+        id: '2',
+        date: new Date('2021-08-15'),
+        requester: '',
+        phone: '',
+        city_id: '',
+        workstation_id: '',
+        email: '',
+        problem_category: {
+          id: '',
+          name: '',
+          description: '',
+          problem_types: [],
+        },
+        problem_types: [],
+      },
     ]);
   });
 
   it('should return the same array when the issues are already sorted', () => {
     const issues: Issue[] = [
-      { id: '1', date: '2021-09-01' },
-      { id: '2', date: '2021-08-30' },
-      { id: '3', date: '2021-08-15' },
+      {
+        id: '1',
+        date: new Date('2021-09-01'),
+        requester: '',
+        phone: '',
+        city_id: '',
+        workstation_id: '',
+        email: '',
+        problem_category: {
+          id: '',
+          name: '',
+          description: '',
+          problem_types: [],
+        },
+        problem_types: [],
+      },
+      {
+        id: '2',
+        date: new Date('2021-08-30'),
+        requester: '',
+        phone: '',
+        city_id: '',
+        workstation_id: '',
+        email: '',
+        problem_category: {
+          id: '',
+          name: '',
+          description: '',
+          problem_types: [],
+        },
+        problem_types: [],
+      },
+      {
+        id: '3',
+        date: new Date('2021-08-15'),
+        requester: '',
+        phone: '',
+        city_id: '',
+        workstation_id: '',
+        email: '',
+        problem_category: {
+          id: '',
+          name: '',
+          description: '',
+          problem_types: [],
+        },
+        problem_types: [],
+      },
     ];
 
     const sortedIssues = sortIssues(issues);
